710c1697653843abd7beff121a78d0b578e01b70
Carlos Clemente <carlos.clemente.abc@gmail.com>
2023-10-29 01:05:40 +0200

    Version v1.0

--- csvTOxsl/Program.cs
+++ csvTOxsl/Program.cs
@@ -1,162 +1,115 @@
-﻿using System.Data;
-using Excel = OfficeOpenXml;
-using Microsoft.VisualBasic.FileIO;
-using OfficeOpenXml;
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using DocumentFormat.OpenXml;
-using DocumentFormat.OpenXml.Spreadsheet;
-using SpreadsheetLight;
-
-class Program
+﻿namespace csvTOxlsx
 {
-    static void Main(string[] args)
+    class Program
     {
-        if (args.Length < 3)
+        static void Main(string[] args)
         {
-            mensajeAyuda();
-        }
+            //Permite ejecutar los metodos como DataTable o como lista (la lista funciona)
 
-        string ficheroCsv = string.Empty;
-        string plantillaExcel = string.Empty;
-        string celdaDestino = string.Empty;
+            metodosLista metodoL = new();
 
-        foreach (string arg in args)
-        {
-            string[] parts = arg.Split('=');
-            if (parts.Length == 2)
+            string ficheroCsv = string.Empty;
+            string ficheroExcel = string.Empty;
+            string plantillaExcel = string.Empty;
+            int hoja = 0;//En la biblioteca NPOI el indice 0 es la primera hoja
+            string celdaDestino = "A1";//Por defecto se pondra en la celda A1
+            int fila = 1;
+            int columna = 1;
+            string textoLog = string.Empty;
+
+            foreach (string arg in args)
             {
-                string key = parts[0].ToLower();
-                string value = parts[1];
-
-                if (key == "origen")
+                if (arg == "-h")
                 {
-                    ficheroCsv = value;
+                    mensajeAyuda();
+                    return;
                 }
-                else if (key == "plantilla")
+                string[] parts = arg.Split('=');
+                if (parts.Length == 2)
                 {
-                    plantillaExcel = value;
-                }
-                else if (key == "celdadestino")
-                {
-                    celdaDestino = value.ToUpper();
-                }
-            }
-        }
+                    string key = parts[0].ToLower();
+                    string value = parts[1];
 
-        if (string.IsNullOrEmpty(ficheroCsv) || string.IsNullOrEmpty(plantillaExcel) || string.IsNullOrEmpty(celdaDestino))
-        {
-            Console.WriteLine("Faltan parámetros o algunos son incorrectos.");
-            mensajeAyuda();
-            return;
-        }
-
-        if (File.Exists(ficheroCsv) && File.Exists(plantillaExcel))
-        {
-            try
-            {
-                DataTable tabla = ReadCsvToDataTable(ficheroCsv);
-                WriteDataToExcel(tabla, plantillaExcel, celdaDestino);
-                //Console.WriteLine("Datos insertados correctamente en Excel.");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error: " + ex.Message);
-            }
-        }
-        else
-        {
-            Console.WriteLine("El archivo CSV o la plantilla de Excel no existen en la ruta especificada.");
-            mensajeAyuda();
-        }
-
-
-        static DataTable ReadCsvToDataTable(string filePath)
-        {
-            //Crea una tabla para insertar los datos que se extraigan del csv
-            DataTable csvData = new DataTable();
-
-            //Se crea una instancia TextFieldParser para tratar el archivo csv
-            using (TextFieldParser parser = new TextFieldParser(filePath))
-            {
-                //Se le indica que los datos estan delimitados y separados por un punto y coma
-                parser.TextFieldType = FieldType.Delimited;
-                parser.SetDelimiters(";");
-                parser.HasFieldsEnclosedInQuotes = true;
-
-                // Lee los datos del csv
-                if (!parser.EndOfData)
-                {
-                    string[] campos = parser.ReadFields();
-                    for (int i = 0; i < campos.Length; i++)
+                    switch (key)
                     {
-                        csvData.Columns.Add(new DataColumn());
-                    }
+                        case "entrada":
+                            ficheroCsv = value;
+                            break;
 
-                    //string[] rowData = parser.ReadFields();
-                    DataRow row = csvData.NewRow();
-                    for (int i = 0; i < campos.Length; i++)
-                    {
-                        row[i] = campos[i];
-                    }
-                    csvData.Rows.Add(row);
-                }
+                        case "salida":
+                            ficheroExcel = value;
+                            break;
 
-                while (!parser.EndOfData)
-                {
-                    string[] rowData = parser.ReadFields();
-                    DataRow row = csvData.NewRow();
-                    for (int i = 0; i < rowData.Length; i++)
-                    {
-                        row[i] = rowData[i];
+                        case "plantilla":
+                            plantillaExcel = value;
+                            break;
+
+                        case "celda":
+                            celdaDestino = value.ToUpper();
+                            int[] columnaFila = metodoL.convertirReferencia(celdaDestino);
+                            fila = columnaFila[1];
+                            columna = columnaFila[0];
+                            break;
+
+                        case "hoja":
+                            hoja = Convert.ToInt32(value);
+                            break;
                     }
-                    csvData.Rows.Add(row);
                 }
             }
 
-            return csvData;
+            //Comprueba que se han pasado como parametro el fichero csv y el nombre del fichero de salida (la plantilla es opcional)
+            if (string.IsNullOrEmpty(ficheroCsv) && string.IsNullOrEmpty(ficheroExcel))
+            {
+                textoLog += "Parametros incorrectos.\n";
+                grabaResultado(textoLog);
+                return;
+            }
+
+            if (hoja < 0)
+            {
+                textoLog += "El numero de hoja no puede ser menor de 1";
+                grabaResultado(textoLog);
+            }
+
+            if (File.Exists(ficheroCsv))
+            {
+                try
+                {
+                    List<List<object>> datos = metodoL.leerCSV(ficheroCsv); //Leer el archivo CSV
+                    textoLog += metodoL.exportaXLSX(datos, plantillaExcel, fila, columna, hoja, ficheroExcel); //Grabar el fichero Excel
+                    grabaResultado(textoLog);
+                }
+                catch (Exception ex)
+                {
+                    textoLog += "Error al procesar los ficheros: " + ex.Message + "\n";
+                    grabaResultado(textoLog);
+                }
+            }
+            else
+            {
+                textoLog += "Los archivos de entrada no estan en la carpeta.\n";
+                grabaResultado(textoLog);
+            }
         }
 
-        static void WriteDataToExcel(DataTable tabla, string plantillaExcel, string celdaDestino)
+        private static void grabaResultado(string textoLog)
         {
-            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-            using (var package = new Excel.ExcelPackage(new FileInfo(plantillaExcel)))
+            //Genera un fichero con el resultado
+            string loggerFich = "resultado.txt";
+            using (StreamWriter logger = new(loggerFich))
             {
-                Excel.ExcelWorksheet hojaExcel = package.Workbook.Worksheets[0];
-
-                int fila = int.Parse(celdaDestino.Substring(1));
-                char columna = celdaDestino[0];
-
-                int columnaIndex = columna - 'A'; // Calcula el índice de la columna en base a la letra
-
-                for (int i = 0; i < tabla.Rows.Count; i++)
-                {
-                    for (int j = 0; j < tabla.Columns.Count; j++)
-                    {
-                        string contenidoCelda = tabla.Rows[i][j].ToString();
-                        contenidoCelda = contenidoCelda.Replace(";", ",");
-                        if (!string.IsNullOrEmpty(contenidoCelda) && contenidoCelda.StartsWith("="))
-                        {
-                            hojaExcel.Cells[fila + i, columnaIndex + j + 1].Formula = contenidoCelda; // +1 porque las columnas de Excel comienzan desde 1
-                        }
-                        else
-                        {
-                            hojaExcel.Cells[fila + i, columnaIndex + j + 1].Value = contenidoCelda;
-                        }
-                    }
-                }
-                //package.Workbook.Calculate();
-                package.Save();
+                logger.WriteLine(textoLog);
             }
         }
 
         static void mensajeAyuda()
         {
-            Console.WriteLine("Parametros incorrectos. Se deben facilitar los siguientes parametros:");
-            Console.WriteLine("csvTOxls origen=archivo.csv plantilla=plantilla.xls celdaDestino=A1");
+            Console.Clear();
+            Console.WriteLine("\nUso de la aplicacion.\n");
+            Console.WriteLine("csvTOexcel [parametro1 parametro2 ... parametroN]\n");
+            Console.WriteLine("Parametros:");
+            Console.WriteLine("\tentrada=archivo.csv (obligatorio)\n\tsalida=archivo.xlsx (obligatorio)\n\tplantilla=plantilla.xlsx (opcional)\n\tcelda=A1 (defecto)\n\thoja=1 (defecto)");
         }
     }
 }
